ae7c0bd8b9cefe347c32106b3c1519380f54dffa
Dhaval Parekh <dmparekh007@gmail.com>
2016-02-10 13:28:36 +0530

    Test bootstrap update

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -7,7 +7,7 @@ define('API_KEY', 'app_secreat_key');
 define('PASSWORD','aw-098@#@Q_Ckpawe94');
 define('SALT','S%#)(vcds;ldWEas@#@');
 
-define('BASE_URL', 'put-your-domain.com'); //Ex `http://domain.com/`
+
 define('INDEX', 'index.php');
 define('DS', DIRECTORY_SEPARATOR);
 
@@ -28,15 +28,12 @@ define('DATABASE', DB_PATH.DB_NAME);
 
 define('CACHE_EXPIRY_TIME',100*60); // time in second // 15 minute
 
-error_reporting(-1 & ~E_DEPRECATED);
-//error_reporting(0);
+error_reporting(-1);
+//error_reporting(-1 & ~E_DEPRECATED);
+
 // Timezone
 date_default_timezone_set('UTC');
-$timezone = '+5.30';
-$timezone = preg_replace('/[^0-9]/', '', $timezone) * 36;
-$timezone_name = timezone_name_from_abbr(null, $timezone, true);
-$timezone_name = $timezone_name?$timezone_name:'Asia/Kolkata';
-date_default_timezone_set($timezone_name);
+
 session_start();
 
 // Load Libraries 
